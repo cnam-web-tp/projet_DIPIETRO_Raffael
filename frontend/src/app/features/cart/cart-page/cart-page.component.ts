40d5e9da83a0101e5f8b57619c051001449b61d0
Raffael Di Pietro <maxou4170@gmail.com>
2024-05-03 14:53:13 +0200

    feat(cart): improve cart display with a summary

--- frontend/src/app/features/cart/cart-page/cart-page.component.ts
+++ frontend/src/app/features/cart/cart-page/cart-page.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  inject
+} from '@angular/core';
 import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { CartState, TramInCart } from '../../../state/cart/cart.state';
 import {
   AddTramToCart,
@@ -13,22 +18,43 @@ import {
 import { TramwayCartCardComponent } from '../../tramways/tramway-cart-card/tramway-cart-card.component';
 import { TramwayCardComponent } from '../../tramways/tramway-card/tramway-card.component';
 import { TuiButtonModule, TuiSvgModule } from '@taiga-ui/core';
+import { CartSummaryComponent } from '../cart-summary/cart-summary.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule, TramwayCardComponent, TuiButtonModule, TuiSvgModule],
+  imports: [
+    CommonModule,
+    TramwayCardComponent,
+    TuiButtonModule,
+    TuiSvgModule,
+    CartSummaryComponent
+  ],
   templateUrl: './cart-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
   private readonly store = inject(Store);
-
   cart$: Observable<TramInCart[]> = this.store.select((state) => state.cart);
+
+  ngOnInit(): void {
+    this.store.dispatch(
+      new AddTramToCart({
+        productId: 1,
+        brand: 'Tramway 1',
+        name: 'Tramway 1',
+        description: 'Tramway 1 description',
+        price: 1000,
+        imageName: 'citadis.webp'
+      })
+    );
+  }
 
   @Select(CartState.getCartTrams) declare tramsInCart$: Observable<
     TramInCart[]
   >;
+
+  @Select(CartState.getCartTotal) declare total$: Observable<number>;
 
   delete(tram: TramInCart): void {
     this.store.dispatch(new DeleteTramFromCart(tram.model));
